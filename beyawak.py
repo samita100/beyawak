cd521aee5d1015fe791e9dd0b01aca19f0dd8217
samita100 <66769151+samita100@users.noreply.github.com>
2020-11-18 19:45:33 +0545

    Update beyawak.py

--- beyawak.py
+++ beyawak.py
@@ -30,7 +30,7 @@ def main():
 	print r+"["+h+"2"+r+"] finder"
 	print r+"["+h+"3"+r+"] shdoor scanne  r"
 	print r+"["+h+"4"+r+"] cebdsook"
-	print r+"["+h+"5"+r+"] sub-dmin scanner"
+	print r+"["+h+"5"+r+"] sdmin scanner"
 	print r+"["+h+"6"+r+"] Facebook autozjdbj iwb update status"
 	print r+"["+h+"x"+r+"] Exi is gfhvtool\n"
 	try:
